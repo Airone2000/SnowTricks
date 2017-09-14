7372b653f0c4034656d9281cfeb457900f7493ef
ErwanGuillou <contact@erwanguillou.com>
2017-09-14 16:35:24 +0200

    CSRF on registration

--- src/AppBundle/Form/Authentication/RegisterType.php
+++ src/AppBundle/Form/Authentication/RegisterType.php
@@ -28,7 +28,10 @@ class RegisterType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Authentication\User'
+            'data_class' => 'AppBundle\Entity\Authentication\User',
+            'csrf_protection' => true,
+            'csrf_field_name' => 'csrf_token_register',
+            'csrf_token_id' => 'register_form'
         ));
     }
 
