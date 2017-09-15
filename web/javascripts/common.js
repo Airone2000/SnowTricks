956a2fd3d98a542a0fe82db9813a4002581efc13
ErwanGuillou <contact@erwanguillou.com>
2017-09-15 15:05:22 +0200

    Footer 1

--- web/javascripts/common.js
+++ web/javascripts/common.js
@@ -10,5 +10,12 @@ $(function(){
             return false;
         }
     });
-
 });
+
+/* Je n'ai pas rédigé les conditions d'utilisation. */
+/* Il est plus propre d'annoncer qqchose que de laisser un lien qui ne mène nulle part */
+function alertCond()
+{
+    alert("Nos conditions générales sont en cours de rédaction.\n\nDécouvrez-les dès leur sortie dans 6 jours.\n\nPour toute question utile, contactez sans hésiter un administrateur : maels1991@gmail.com");
+    return false;
+}
