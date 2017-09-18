67390e31033f6a50149e0826f0f3de996c2ebe16
Erwan Guillou <contact@erwanguillou.com>
2017-09-18 13:52:59 +0200

    Update PasswordRecoveryPostPersist.php
    Email not sending if not sender defined.

--- src/AppBundle/Listener/Authentication/PasswordRecoveryPostPersist.php
+++ src/AppBundle/Listener/Authentication/PasswordRecoveryPostPersist.php
@@ -25,8 +25,13 @@ class PasswordRecoveryPostPersist
         $entity = $event->getEntity();
         if($entity instanceof PasswordRecovery)
         {
-            $message = new \Swift_Message('SnowTricks : récupération de votre mot de passe','<a href="'. $this->router->generate('reset_password', ['token' => $entity->getToken()], UrlGeneratorInterface::ABSOLUTE_URL) .'">Cliquez ici pour le réinitialiser</a>');
-            $this->mailer->send( $message->setTo($entity->getEmail()) );
+            $message = new \Swift_Message();
+            $message->setSubject('SnowTricks : Récupération de votre mot de passe');
+            $message->setFrom('contact@erwanguillou.com');
+            $message->setTo( $entity->getEmail() );
+            $message->setBody('<a href="'. $this->router->generate('reset_password', ['token' => $entity->getToken()], UrlGeneratorInterface::ABSOLUTE_URL) .'">Cliquez ici pour réinitialiser votre mot de passe</a>', 'text/html');
+
+            $this->mailer->send($message);
 
             $this->session->getFlashBag()->add('success', 'Un Email vient de vous être expédié.');
         }
