779ddb9775f8e2dcbd949f6fb766c5771dba0fc2
Erwan Guillou <contact@erwanguillou.com>
2017-09-18 14:08:38 +0200

    Update InitCommand.php
    Adding some comments inside.

--- src/AppBundle/Command/InitCommand.php
+++ src/AppBundle/Command/InitCommand.php
@@ -16,6 +16,8 @@ class InitCommand extends Command
 
     public function __construct($name = null, KernelInterface $kernel)
     {
+        # Ici, j'injecte le kernel car il sera indispensable pour le suite.
+        # Voir $this->execute()
         $this->kernel = $kernel;
         parent::__construct($name);
     }
@@ -36,12 +38,17 @@ class InitCommand extends Command
         ]);
 
         $application = new Application($this->kernel);
+        # Sans cette instruction, la commande risquerait de rendre la main sans préavis.
         $application->setAutoExit(false);
         $buffer = new BufferedOutput();
 
+        # Première sous-commande : créer la base de données et conserver
+        # la sortie dans un buffer.
         $input = new ArrayInput(['command' => 'doctrine:database:create']);
         $application->run($input, $buffer);
 
+        # La seconde étape (sous-commande) consiste à créer la structure de la base de données
+        # à partir des entités.
         $input = new ArrayInput(['command' => 'doctrine:schema:update', '--force' => true]);
         $application->run($input, $buffer);
         $output->writeln([
@@ -50,6 +57,9 @@ class InitCommand extends Command
             "Chargement des données ..."
         ]);
 
+        # Troisième étape : on charge les fixtures.
+        # On évite toute intéraction. En effet, les intéractions (demande de confirmation)
+        # sont en anglais. Cela ferait tâche de mélanger anglais et français.
         $input = new ArrayInput(['command' => 'doctrine:fixtures:load', '--no-interaction' => true]);
         $input->setInteractive(false);
         $application->run($input, $buffer);
